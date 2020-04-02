37bcbf6674202d80947e9ba7cb56a963ac9261ab
vsapiens <erickfrank@icloud.com>
2020-04-01 19:29:59 -0600

    Added Proyect

---
+++ QuimicaApp/QuimicaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuimicaApp
+//
+//  Created by Erick González on 01/04/20.
+//  Copyright © 2020 Erick González. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
